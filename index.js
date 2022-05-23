ba827c1b095f0203552b57e5f2c7327e6833c496
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-24 04:21:08 +0600

    connected to the mongodb and loaded data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
@@ -8,10 +9,36 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// CONNECTING TO DATABASE
+
+
+const uri = `mongodb+srv://household-user:O1rjWwEbPxDPyv6M@cluster0.otz5f.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("household").collection("products");
+
+        app.get('/products', async(req, res)=>{
+            const query = {};
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
