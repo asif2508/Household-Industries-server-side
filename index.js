5134d56cf2512fda61fdc29502137019e7ca4f80
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-24 15:11:05 +0600

    user collection and jwt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express()
@@ -20,8 +21,23 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db("household").collection("products");
+        const userCollection = client.db("household").collection("users");
 
-        app.get('/products', async(req, res)=>{
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({email:email}, process.env.WEB_TOKEN_SECRET, { expiresIn: '1d' });
+            
+            res.send(result, token);
+        })
+
+        app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const result = await cursor.toArray();
